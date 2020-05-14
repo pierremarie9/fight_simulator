8cc43d56ff3945c73be15f7f66d721a5fe0b2784
pierremarie9 <63357279+pierremarie9@users.noreply.github.com>
2020-05-14 16:13:23 +0200

    jauge de vie
    implémentation de la jauge de vie

--- fight.js
+++ fight.js
@@ -1,18 +1,22 @@
-let atkBtnR = document.querySelector("#attackRocky");
-let atkBtnI = document.querySelector("#attackIvan");
-let startBtn = document.querySelector("#start_button");
-let dfcBtnR = document.querySelector("#defendRocky");
-let msgBox  = document.querySelector("#msgbox");
-let santeRocky = document.querySelector("#vieRocky");
-let santeIvan = document.querySelector("#vieIvan");
+let atkBtnR     = document.querySelector("#attackRocky");
+let atkBtnI     = document.querySelector("#attackIvan");
+let startBtn    = document.querySelector("#start_button");
+let dfcBtnR     = document.querySelector("#defendRocky");
+let spcBtnR     = document.querySelector("#specialRocky");
+let msgBox      = document.querySelector("#msgbox");
+let santeRocky  = document.querySelector("#vieRocky");
+let santeIvan   = document.querySelector("#vieIvan");
+let jaugeRocky  = document.querySelector("#avancementR");
+let jaugeIvan   = document.querySelector("#avancementI");
 
 class Personnage {
-    constructor(pseudo, classe, sante, attaque, defaite){
+    constructor(pseudo, classe, sante, attaque, defaite, jauge){
         this.pseudo  = pseudo;
         this.classe  = classe;
         this.sante   = sante;
         this.attaque = attaque;
         this.defaite = defaite;
+        this.jauge   = jauge;
     }
 
 
@@ -31,7 +35,7 @@ class Personnage {
 class Star extends Personnage {
 
     constructor(pseudo){
-        super(pseudo, "star", 200, 50, false);
+        super(pseudo, "star", 200, 50, false, 0);
     }
 
     attaquer(personnage){
@@ -43,13 +47,28 @@ class Star extends Personnage {
 
     defendre(personnage){
         this.sante += Math.floor(Math.random()*40) + 1;
+        if(this.jauge <4){
+            this.jauge ++;
+        }
         return this.pseudo + " esquive l'attaque de " + personnage.pseudo + " ses points de vie montent desormais à  " + this.sante
     }
 
     coupSpecial(personnage){
+        this.jauge = 0;
         personnage.sante -= 60;
-        console.log(this.pseudo + " attaque avec son coup spécial uppercut de l'espace " + personnage.pseudo + " (" + 5*this.attaque + " degats)");
         personnage.verifierSante();
+        return this.pseudo + " attaque avec son coup spécial uppercut de l'espace " + personnage.pseudo + " ( 60 degats)";
+    }
+
+    checkJauge(){
+        if (this.jauge < 4){
+            jaugeRocky.value = this.jauge;
+        }else if(this.jauge == 4){
+            spcBtnR.style.display = 'block';
+            jaugeRocky.value = this.jauge;
+        }else{
+            spcBtnR.style.display = 'block';
+        }
     }
 
 }
@@ -57,7 +76,7 @@ class Star extends Personnage {
 class Rival extends Personnage {
 
     constructor(pseudo){
-        super(pseudo, "rival", 200, 50, false);
+        super(pseudo, "rival", 200, 50, false, 0);
     }
 
     attaquer(personnage){
@@ -69,26 +88,47 @@ class Rival extends Personnage {
 
     defendre(personnage){
         this.sante += Math.floor(Math.random()*40) +1;
+        if(this.jauge <4){
+            this.jauge ++;
+        }
         return this.pseudo + " esquive l'attaque de " + personnage.pseudo + " ses points de vie montent desormais à  " + this.sante
     }
 
     coupSpecial(personnage){
         personnage.sante -= 60;
-        console.log(this.pseudo + " attaque avec son coup spécial: coup de genou dans la mâchoire " + personnage.pseudo + " (" + 5*this.attaque + " degats)");
         personnage.verifierSante();
+        return this.pseudo + " attaque avec son coup spécial: coup de genou dans la mâchoire " + personnage.pseudo + " (60 degats)";
     }
 
     jouer(){
-        if(this.sante > 0){
-            let hasard = Math.floor(Math.random()*2);
-            if(hasard < 1){
+        if(this.jauge == 4){
+
+            this.jauge = 0;
+            return this.coupSpecial(rocky);
+            
+
+        }else{
+
+            if(this.sante > 0){
+                let hasard = Math.floor(Math.random()*2);
+                if(hasard < 1){
+                    return this.attaquer(rocky);
+                }else{
+                    return this.defendre(rocky);
+                }
+            }
+            else{
                 return this.attaquer(rocky);
-            }else{
-                return this.defendre(rocky);
             }
         }
-        else{
-            return this.attaquer(rocky);
+           
+    }
+
+    checkJauge(){
+        if (this.jauge < 4){
+            jaugeIvan.value = this.jauge;
+        }else if(this.jauge == 4){
+            jaugeIvan.value = this.jauge;
         }
     }
 
@@ -97,6 +137,7 @@ class Rival extends Personnage {
 rocky = new Star("Rocky");
 ivan = new Rival("Ivan");
 
+spcBtnR.style.display = 'none';
 santeRocky.textContent = rocky.sante;
 santeIvan.textContent  = ivan.sante;
 var textRocky = document.createElement('p');
@@ -162,6 +203,8 @@ atkBtnR.addEventListener('click', ()=>{
             santeIvan.textContent  = ivan.sante;
             atkBtnR.style.display = 'block';
             dfcBtnR.style.display = 'block';
+            rocky.checkJauge();
+            ivan.checkJauge();
         }, 3000);
     }
 
@@ -179,6 +222,7 @@ atkBtnR.addEventListener('click', ()=>{
 dfcBtnR.addEventListener('click', ()=>{
     atkBtnR.style.display = 'none';
     dfcBtnR.style.display = 'none';
+    spcBtnR.style.display = 'none';
     let instruction = document.createElement('div');
     instruction.innerHTML = rocky.defendre(ivan) + "<br />" + ivan.informations;
     instruction.style.backgroundColor = 'rgba(0, 51, 153, 0.2)';
@@ -197,11 +241,51 @@ dfcBtnR.addEventListener('click', ()=>{
             santeIvan.textContent  = ivan.sante;
             atkBtnR.style.display = 'block';
             dfcBtnR.style.display = 'block';
+            rocky.checkJauge();
+            ivan.checkJauge();
         }, 3000);
     };
 
     if(rocky.sante == 0 || ivan.sante == 0){
         setTimeout(() =>{
+            santeRocky.textContent = rocky.sante;
+            santeIvan.textContent  = ivan.sante;
+            afficheFin(rocky.sante, ivan.sante)
+        }, 3000);
+    };
+
+})
+
+spcBtnR.addEventListener('click', ()=>{
+    atkBtnR.style.display = 'none';
+    dfcBtnR.style.display = 'none';
+    spcBtnR.style.display = 'none';
+    let instruction = document.createElement('div');
+    instruction.innerHTML = rocky.coupSpecial(ivan) + "<br />" + ivan.informations;
+    instruction.style.backgroundColor = 'rgba(0, 51, 153, 0.2)';
+    msgBox.prepend(instruction);
+    santeRocky.textContent = rocky.sante;
+    santeIvan.textContent  = ivan.sante;
+
+    let resultIvan = ivan.jouer();
+    let instructionI = document.createElement('div');
+    instructionI.style.backgroundColor = 'rgba(255, 51, 0, 0.2)';
+    instructionI.innerHTML = resultIvan + "<br />" + rocky.informations;
+    setTimeout(() => { msgBox.prepend(instructionI); }, 3000);
+    if(rocky.sante != 0 && ivan.sante !=0){
+        setTimeout(() => { 
+            santeRocky.textContent = rocky.sante;
+            santeIvan.textContent  = ivan.sante;
+            atkBtnR.style.display = 'block';
+            dfcBtnR.style.display = 'block';
+            rocky.checkJauge();
+            ivan.checkJauge();
+        }, 3000);
+    }
+
+    if(rocky.sante == 0 || ivan.sante == 0){
+
+        setTimeout(() =>{ 
             santeRocky.textContent = rocky.sante;
             santeIvan.textContent  = ivan.sante;
             afficheFin(rocky.sante, ivan.sante)
